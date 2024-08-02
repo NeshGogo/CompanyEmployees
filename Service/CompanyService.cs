90db89ef7e10546434931d4824e6a2bda0c43b48
neshgogo <rafaelaguero10@hotmail.com>
2024-08-02 10:51:20 -0400

    Adding a service layer

---
+++ Service/CompanyService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+internal sealed class CompanyService : ICompanyService
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+
+    public CompanyService(IRepositoryManager repositoryManager, ILoggerManager logger)
+    {
+        _repository = repositoryManager;
+        _logger = logger;
+    }
+}
