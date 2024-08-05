5d6c34a622ac690cf46e0358609ed5f0768b7b0b
neshgogo <rafaelaguero10@hotmail.com>
2024-08-05 08:51:23 -0400

    Controllers and Routing

---
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+}
