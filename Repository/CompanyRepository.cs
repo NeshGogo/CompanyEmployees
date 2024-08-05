1246fb7a9d88e5c0ca549eaf4b315363055f2273
neshgogo <rafaelaguero10@hotmail.com>
2024-08-05 09:49:36 -0400

    Getting all companies from the database

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -8,4 +8,9 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) => 
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
 }
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using Service.Contracts;
 
 namespace Service;
@@ -12,5 +13,19 @@ internal sealed class CompanyService : ICompanyService
     {
         _repository = repositoryManager;
         _logger = logger;
+    }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    {
+        try
+        {
+            var companies = _repository.Company.GetAllCompanies(trackChanges);
+            return companies;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"--> Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
+            throw;
+        }
     }
 }
