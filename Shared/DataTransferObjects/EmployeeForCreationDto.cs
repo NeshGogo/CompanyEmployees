1c72e283d4ebfd938fb2ea2ef41d66fbb464a700
NeshGogo <rafaelaguero10@hotmail.com>
2024-08-07 21:37:48 -0400

    Validating Int Type

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -7,6 +7,7 @@ public record EmployeeForCreationDto(
     [MaxLength(30, ErrorMessage = "Maximun length for the Name is 30 characters.")]
     string Name,
     [Required(ErrorMessage = "Employee age is a required field")]
+    [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")]
     int Age,
     [Required(ErrorMessage = "Employee position is a required field")]
     [MaxLength(30, ErrorMessage = "Maximun length for the Position is 20 characters.")]
