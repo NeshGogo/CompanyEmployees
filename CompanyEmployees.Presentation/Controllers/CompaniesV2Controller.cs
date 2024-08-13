8582015f4cc9f58e773feaa260eb1a80ae0181b2
neshgogo <rafaelaguero10@hotmail.com>
2024-08-13 09:43:46 -0400

    Deprecated version

--- CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -4,7 +4,7 @@ using Service.Contracts;
 
 namespace CompanyEmployees.Presentation.Controllers;
 
-[ApiVersion("2.0")]
+[ApiVersion("2.0", Deprecated = true)]
 [Route("api/companies")]
 [ApiController]
 public class CompaniesV2Controller : ControllerBase
