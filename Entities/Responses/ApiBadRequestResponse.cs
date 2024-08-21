c9b4710f0483d5b36d8b24c5b363b0b3aa3be6c7
NeshGogo <rafaelaguero10@hotmail.com>
2024-08-20 21:42:38 -0400

    Adding response classes to the project

---
+++ Entities/Responses/ApiBadRequestResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Responses;
+
+public abstract class ApiBadRequestResponse : ApiBaseResponse
+{
+    public string Message { get; set; }
+    public ApiBadRequestResponse(string message)
+        : base(false)
+    {
+        Message = message;
+    }
+}
