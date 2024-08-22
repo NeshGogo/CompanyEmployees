92f9a378a1894bd7d9a711e9d1260cc1979de8a6
neshgogo <rafaelaguero10@hotmail.com>
2024-08-22 16:48:51 -0400

    Working in requests with mediatR

---
+++ Application/Handlers/GetCompaniesHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Queries;
+using Contracts;
+using MediatR;
+using Shared.DataTransferObjects;
+
+namespace Application.Handlers;
+
+internal sealed class GetCompaniesHandler : IRequestHandler<GetCompaniesQuery, IEnumerable<CompanyDto>>
+{
+    private readonly IRepositoryManager _repository;
+
+    public GetCompaniesHandler(IRepositoryManager repository) => _repository = repository;
+
+    public Task<IEnumerable<CompanyDto>> Handle(GetCompaniesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/Queries/GetCompaniesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared.DataTransferObjects;
+
+namespace Application.Queries;
+
+public sealed record class GetCompaniesQuery(bool TrackChanges) : IRequest<IEnumerable<CompanyDto>>;
